3b9d08cf93e81f40a5aac3ff04b72fdc77fcacae
CHHUEN CHIHAY <hay.mario168@gmail.com>
2024-12-04 10:46:44 +0700

    create header and NavButton component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,67 @@
 <template>
-  <RouterView />
+
+  <div class="header">
+    <div class="upper">
+      <div class="brand">
+        <img width="156.25rem" height="161.25rem" src="../public/images/logo.png" alt="">
+        <p>SKIN GLOW</p>
+      </div>
+
+      <div class="options">
+        <input type="Search" placeholder="Search">
+        <button>cart</button>
+        <button>favorite</button>
+        <button>Information</button>
+      </div>
+
+    </div>
+
+    <div class="lower">
+      <NavButton></NavButton>
+    </div>
+  </div>
+
+  <!-- <RouterView /> -->
+
+  <!-- <Footer></Footer> -->
+
 </template>
 <script>
+import NavButton from './components/NavButton.vue';
+
 export default {
   name: 'App',
+  components: {
+    NavButton,
+  }
 };
 </script>
+
+<style scoped>
+
+.brand{
+  display: flex;
+  align-items: center;
+}
+
+.upper{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header{
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: #FFCBCF;
+}
+
+.lower{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
+</style>
---
+++ src/components/NavButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button @click="shopNow()" class="button" :style="{ backgroundColor: color}">
+        <p> {{name}} Home </p>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "NavButton",
+    props: ["color", 'name'],
+    
+
+    methods: {
+        shopNow() {
+            alert("Let's shop! ");
+        }
+    },
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
+.button{
+    /* font-family: 'Poppins', sans-serif; */
+}
+</style>
