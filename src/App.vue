4e048048c94d652cd4643c117cd68ba545641f27
Chum Nita <chumnita0312@gmail.com>
2024-12-04 15:30:48 +0700

    gridProduct component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
-<template>
+<!-- <template>
   <RouterView />
+</template> -->
+<template>
+  <!-- <RouterView /> -->
+
+  <div id="app">
+    <ProductGrid />
+  </div>
 </template>
+
+
+<style>
+/* Add any global styles */
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: white;
+}
+</style>
+
 <script>
+import ProductGrid from './views/ProductGrid.vue';
+
 export default {
   name: 'App',
+  components: {
+    ProductGrid,
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import './assets/main.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from "./router/index.js";
-
+// createApp(App).mount('#app');
 const app = createApp(App)
 
 app.use(createPinia())
