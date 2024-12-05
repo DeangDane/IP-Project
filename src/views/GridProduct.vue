65b356fcfcd69fd566a6fc8f9ab34011c4c4b475
Chum Nita <chumnita0312@gmail.com>
2024-12-05 16:51:30 +0700

    Gridproduct

---
+++ src/views/GridProduct.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="grid-container">
+      <ProductCard
+        v-for="(product, index) in products"
+        :key="index"
+        :product="product"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import ProductCard from '../components/Productcard.vue';
+  
+  export default {
+    name: 'ProductGrid',
+    components: {
+      ProductCard,
+    },
+    data() {
+      return {
+        products: [
+          {
+            title: 'Square One',
+            image: 'https://www.kiehls.com.ph/wp-content/uploads/2023/05/kiehls-face-ultra-pure-high-potency-serum-hyaluronic-acid-30ml-3605972791313-front-1-570x670.jpg',
+            price: '$18.9',
+            rating: 4,
+            isHot: true,
+          },
+          // Add more products
+        ],
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .grid-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 12px;
+    padding: 10px;
+    max-width: 560px;
+    margin: auto;
+  }
+  </style>
+  
