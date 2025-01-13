d0fba4164475f929fd102ed32d0a4348c1c4b277
CHHUEN CHIHAY <hay.mario168@gmail.com>
2025-01-13 10:25:43 +0700

    adjustement

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,52 +1,49 @@
 <template>
   <div class="login-container">
-      <div class="left-panel">
-          <img class="logo" src="@/assets/logo.png" alt="Skin Glow Logo" />
-          <h1>Skin Glow</h1>
-      </div>
+    <div class="left-panel">
+      <img class="logo" src="@/assets/logo.png" alt="Skin Glow Logo" />
+      <h1>Skin Glow</h1>
+    </div>
 
-      <div class="right-panel">
-          <div class="login-form">
-              <h2>Create an account</h2>
-              <form @submit.prevent="handleSignUp">
-                  <div class="input-group">
-                      <label for="name">Name</label>
-                      <input type="text" id="name" v-model="name" required />
-                  </div>
-                  <div v-if="signUpError.name" class="error-message">{{ signUpError.name }}</div>
-                  <div class="input-group">
-                      <label for="email">Email</label>
-                      <input type="email" id="email" v-model="email" required />
-                  </div>
-                  <div v-if="signUpError.email" class="error-message">{{ signUpError.email }}</div>
-                  <div class="input-group">
-                      <label for="password">Password</label>
-                      <input type="password" id="password" v-model="password" required />
-                  </div>
-                  <div class="input-group">
-                      <label for="password">Confirm Password</label>
-                      <input type="password" id="confirmpassword" v-model="confirmpassword" required />
-                  </div>
-
-                  <div v-if="signUpError.password" class="error-message">{{ signUpError.password }}</div>
-                  <div v-if="signUpError.confirmpassword" class="error-message">{{ signUpError.confirmpassword }}
-                  </div>
-
-                  <button type="submit" class="login-btn">Sign Up</button>
-              </form>
-              <div class="social-login">
-                  <p>Continue as</p>
-                  <font-awesome-icon :icon="['fab', 'facebook']" size="3x"
-                      :style="{ color: 'blue', padding: '10px' }" />
-                  <font-awesome-icon :icon="['fab', 'google']" size="3x" :style="{ color: 'red', padding: '10px' }" />
-                  <font-awesome-icon :icon="['fab', 'apple']" size="4x"
-                      :style="{ color: 'black', padding: '10px' }" />
-              </div>
-              <p class="signup-link">
-                  Already have an account? <a href="#" @click.prevent="$emit('switchToLogin')">Login</a>
-              </p>
+    <div class="right-panel">
+      <div class="login-form">
+        <h2>Create an account</h2>
+        <form @submit.prevent="handleSignUp">
+          <div class="input-group">
+            <label for="name">Name</label>
+            <input type="text" id="name" v-model="name" required />
           </div>
+          <div v-if="signUpError.name" class="error-message">{{ signUpError.name }}</div>
+          <div class="input-group">
+            <label for="email">Email</label>
+            <input type="email" id="email" v-model="email" required />
+          </div>
+          <div v-if="signUpError.email" class="error-message">{{ signUpError.email }}</div>
+          <div class="input-group">
+            <label for="password">Password</label>
+            <input type="password" id="password" v-model="password" required />
+          </div>
+          <div class="input-group">
+            <label for="password">Confirm Password</label>
+            <input type="password" id="confirmpassword" v-model="confirmpassword" required />
+          </div>
+
+          <div v-if="signUpError.password" class="error-message">{{ signUpError.password }}</div>
+          <div v-if="signUpError.confirmpassword" class="error-message">{{ signUpError.confirmpassword }}</div>
+
+          <button type="submit" class="login-btn">Sign Up</button>
+        </form>
+        <div class="social-login">
+          <p>Continue as</p>
+          <font-awesome-icon :icon="['fab', 'facebook']" size="3x" :style="{ color: 'blue', padding: '10px' }" />
+          <font-awesome-icon :icon="['fab', 'google']" size="3x" :style="{ color: 'red', padding: '10px' }" />
+          <font-awesome-icon :icon="['fab', 'apple']" size="4x" :style="{ color: 'black', padding: '10px' }" />
+        </div>
+        <p class="signup-link">
+          Already have an account? <a href="#" @click.prevent="$emit('switchToLogin')">Login</a>
+        </p>
       </div>
+    </div>
   </div>
 </template>
 <script>
@@ -54,38 +51,38 @@ import { RouterLink } from 'vue-router';
 
 export default {
   data() {
-      return {
-          name: "",
-          email: "",
-          password: "",
-          confirmpassword: "",
-          signUpError: {}, // Error messages for invalid signup
-      };
+    return {
+      name: "",
+      email: "",
+      password: "",
+      confirmpassword:"",
+      signUpError: {}, // Error messages for invalid signup
+    };
   },
   methods: {
-      handleSignUp() {
-          this.signUpError = {};
-          if (!this.name.trim()) {
-              this.signUpError.name = "Name is required.";
-          }
-          if (!this.email.includes("@")) {
-              this.signUpError.email = "Invalid email format.";
-          }
-          if (this.password.length < 6) {
-              this.signUpError.password = "Password must be at least 6 characters.";
-          }
-          if (this.confirmpassword.length < 6) {
-              this.signUpError.confirmpassword = "Confirm Password must be the same Password.";
-          }
-          if (!Object.keys(this.signUpError).length) {
-              alert("Sign Up Successful!");
-              const userProfileStore = useUserProfileStore();
-              userProfileStore.saveUserProfile({ name: this.name, email: this.email });
-              this.$emit('switchToLogin');
-              // Navigate to the login page
-              // this.$router.push("/login");
-          }
-      },
+    handleSignUp() {
+      this.signUpError = {};
+      if (!this.name.trim()) {
+        this.signUpError.name = "Name is required.";
+      }
+      if (!this.email.includes("@")) {
+        this.signUpError.email = "Invalid email format.";
+      }
+      if (this.password.length < 6) {
+        this.signUpError.password = "Password must be at least 6 characters.";
+      }
+      if (this.confirmpassword.length < 6) {
+        this.signUpError.confirmpassword = "Confirm Password must be the same Password.";
+      }
+      if (!Object.keys(this.signUpError).length) {
+        alert("Sign Up Successful!");
+        const userProfileStore = useUserProfileStore();
+        userProfileStore.saveUserProfile({ name: this.name, email: this.email });
+        this.$emit('switchToLogin');
+        // Navigate to the login page
+        // this.$router.push("/login");
+      }
+    },
   },
 };
 </script>
@@ -100,7 +97,7 @@ export default {
 
 .left-panel {
   flex: 1;
-  background-color: #f9dada;
+  background-color: #f9dada; 
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -110,8 +107,7 @@ export default {
 }
 
 .left-panel img.logo {
-  width: 210px;
-  /* Adjust logo size */
+  width: 210px; /* Adjust logo size */
   margin-bottom: 5px;
 }
 
@@ -193,8 +189,7 @@ export default {
 
 .options .forgot-password {
   font-size: 14px;
-  color: #f08c9c;
-  /* Soft pink */
+  color: #f08c9c; /* Soft pink */
   text-decoration: none;
 }
 
@@ -205,8 +200,7 @@ export default {
 .login-btn {
   width: 100%;
   padding: 10px;
-  background-color: #f08c9c;
-  /* Soft pink */
+  background-color: #f08c9c; /* Soft pink */
   color: #fff;
   border: none;
   border-radius: 5px;
@@ -233,8 +227,7 @@ export default {
 }
 
 .signup-link a {
-  color: #f08c9c;
-  /* Soft pink */
+  color: #f08c9c; /* Soft pink */
   text-decoration: none;
 }
 
@@ -242,3 +235,4 @@ export default {
   text-decoration: underline;
 }
 </style>
+  
